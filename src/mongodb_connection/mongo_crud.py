68dc41aeb664ee382b2d8d3eb57c935205d9562e
Ashutosh Tiwari <ashutosh.formin@gmail.com>
2024-01-23 02:27:14 +0530

    update version code

--- src/mongodb_connection/mongo_crud.py
+++ src/mongodb_connection/mongo_crud.py
@@ -1,4 +1,11 @@
-# pipeline for the automate the mongo_db operation
+from typing import Any
+import os
+import pandas as pd
+import pymongo
+import json
+from ensure import ensure_annotations
+
+
 from typing import Any
 import os
 import pandas as pd
@@ -6,50 +13,59 @@ from pymongo.mongo_client import MongoClient
 import json
 from ensure import ensure_annotations
 
-class mongodb_operation:
 
-    def __init__(self, client_url : str, database_name: str, collection_name: str= None):
-        self.client_url = client_url
-        self.database_name = database_name
-        self.collection_name = collection_name
-
-    def create_client(self):
-        client=MongoClient(self.client_url)
-        return client 
+class mongo_operation:
+    __collection=None # here i have created a private/protected variable
+    __database=None
     
-    def create_database(self):
-        client = self.create_client()
-        database = client[self.database_name]
-        return database
-
-    def create_collection(self, collection = None):
-        database = self.create_database()
-        collection = database[self.collection_name]
-        return collection
-
-
-    def insert_record(self, record: dict, collection_name:str):
+    def __init__(self,client_url: str, database_name: str, collection_name: str=None):
+        self.client_url=client_url
+        self.database_name=database_name
+        self.collection_name=collection_name
+       
+    def create_mongo_client(self,collection=None):
+        client=MongoClient(self.client_url)
+        return client
+    
+    def create_database(self,collection=None):
+        if mongo_operation.__database==None:
+            client=self.create_mongo_client(collection)
+            self.database=client[self.database_name]
+        return self.database 
+    
+    def create_collection(self,collection=None):
+        if mongo_operation.__collection==None:
+            database=self.create_database(collection)
+            self.collection=database[self.collection_name]
+            mongo_operation.__collection=collection
+        
+        if mongo_operation.__collection!=collection:
+            database=self.create_database(collection)
+            self.collection=database[self.collection_name]
+            mongo_operation.__collection=collection
+            
+        return self.collection
+    
+    def insert_record(self,record: dict, collection_name: str) -> Any:
         if type(record) == list:
             for data in record:
                 if type(data) != dict:
-                    raise TypeError("record must be in the list")
-            collection = self.create_collection(collection_name)
+                    raise TypeError("record must be in the dict")    
+            collection=self.create_collection(collection_name)
             collection.insert_many(record)
-            
-        elif type(record)== dict:
-            collection = self.create_collection(collection_name)
+        elif type(record)==dict:
+            collection=self.create_collection(collection_name)
             collection.insert_one(record)
-
-
-    def bulk_insert(self, datafile:str, collection_name: str):
-        self.path = datafile
-
+    
+    def bulk_insert(self,datafile,collection_name:str=None):
+        self.path=datafile
+        
         if self.path.endswith('.csv'):
-            data = pd.read_csv(self.path , encoding = 'utf-8')
-
-        elif self.path.endswith('.xlsx'):
-            data = pd.read_excel(self.path, encoding = 'utf-8')    
-
-        datajson = json.loads(data.to_json(orient = 'record'))
-        collection = self.create_collection()
+            pd.read.csv(self.path,encoding='utf-8')
+            
+        elif self.path.endswith(".xlsx"):
+            dataframe=pd.read_excel(self.path,encoding='utf-8')
+            
+        datajson=json.loads(dataframe.to_json(orient='record'))
+        collection=self.create_collection()
         collection.insert_many(datajson)
