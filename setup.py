990090a71486451e0c183b208299a42c2896801b
Ashutosh Tiwari <ashutosh.formin@gmail.com>
2024-01-23 02:35:41 +0530

    update version code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.3"
 REPO_NAME = "mongodbconnectorpkg"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "Ashut90 "
