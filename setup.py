86aba912343f21a4b47ac0f96a72755bd0b1e889
Ashutosh Tiwari <ashutosh.formin@gmail.com>
2024-01-25 15:58:17 +0530

    version updated

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = "0.0.4"
 REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "Ashut90"
-AUTHOR_EMAIL = "ashutosh.formin@gmail.com"
+PKG_NAME= "database-automate"
+AUTHOR_USER_NAME = "Ashutosh94"
+AUTHOR_EMAIL = "ash945512@gmail.com"
 
 setup(
     name=PKG_NAME,
