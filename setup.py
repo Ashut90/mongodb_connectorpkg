8612c9396dab38b0cb62371ca10c570e906ae4db
Ashutosh Tiwari <ashutosh.formin@gmail.com>
2024-01-25 15:26:26 +0530

    version updated

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author_email=AUTHOR_EMAIL,
     description="A python package for connecting with database.",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
