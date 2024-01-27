d64d675af36c192a14aac97de5c4e8d26dc9291c
Ashutosh Tiwari <ashutosh.formin@gmail.com>
2024-01-27 23:33:37 +0530

    update profiling

--- setup.py
+++ setup.py
@@ -25,4 +25,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires = ["pymongo","pymongo[srv]","dnspython","pandas","numpy","ensure","pytest"]
     )
